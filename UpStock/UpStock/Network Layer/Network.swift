a4250e640af831341c71038a3b245fb0a0f73c4f
oguuk <ogw135@gmail.com>
2023-07-31 22:52:14 +0900

    feat: 네트워크 클래스에 HTTP GET 요청 처리를 위한 get 메서드 추가

---
+++ UpStock/UpStock/Network Layer/Network.swift
@@ -0,0 +1,33 @@
+//
+//  Network.swift
+//  UpStock
+//
+//  Created by 오국원 on 2023/07/28.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+final class Network {
+    
+    static let `default` = Network()
+    
+    func get(url: String, parameters: [String : Any] = [:]) -> Observable<Result<Data?, AFError>> {
+        return Observable.create { observer in
+            let request = AF.request(url,
+                                     method: .get,
+                                     parameters: parameters)
+                            .validate()
+                            .response { response in
+                                switch response.result {
+                                case .success(let data):
+                                    observer.onNext(.success(data))
+                                case .failure(let error):
+                                    observer.onNext(.failure(error))
+                                }
+                                observer.onCompleted()
+                            }
+            return Disposables.create { request.cancel() }
+        }
+    }
