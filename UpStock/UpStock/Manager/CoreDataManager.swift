9f2c61bd22579d527b2bcb21f9127dbbf0d4102b
oguuk <ogw135@gmail.com>
2023-08-04 18:08:32 +0900

    feat : CoreData에 저장하기 위한 CoreDataManager save메서드 구현

---
+++ UpStock/UpStock/Manager/CoreDataManager.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataManager.swift
+//  UpStock
+//
+//  Created by 오국원 on 2023/07/31.
+//
+
+import UIKit
+import CoreData
+
+final class CoreDataManager {
+    
+    private let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    
+    static var `default` = CoreDataManager()
+    
+    func save<T>(forEntityName: String, value: T) {
+        guard let context = context,
+              let entity = NSEntityDescription.entity(forEntityName: forEntityName, in: context) else { return }
+        
+        let mirror = Mirror(reflecting: value)
+        
+        guard let uniqueValue = mirror.descendant("koreanName") as? String else {
+            print("Data does not have 'koreanName'.")
+            return
+        }
+        
+        if existEntityWith(uniqueValue: uniqueValue, entityName: forEntityName) { return }
+        
+        let newData = NSManagedObject(entity: entity, insertInto: context)
+        
+        mirror.children.forEach { (k, v) in
+            guard let k else { return }
+            newData.setValue(v, forKey: k)
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            print("Failed to save data: \(error)")
+        }
+    }
