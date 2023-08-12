53f3e725e0b6f29040ff222b922ad3b9b2fcfd0f
oguuk <ogw135@gmail.com>
2023-08-12 23:49:51 +0900

    feat : 기본적인 UI 레이아웃 설정

--- UpStock/UpStock/Presenter/Common/StockListCell.swift
+++ UpStock/UpStock/Presenter/Common/StockListCell.swift
@@ -72,3 +72,68 @@ final class StockListCell: UICollectionViewCell {
         coinName.text = nil
         change.image = nil
     }
+    
+    func configureUI() {
+        [change, coinName, market,
+         price, priceFluctuation, tradingVolume,
+         fluctuationRate].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview($0)
+        }
+        
+        [coinName, market].forEach {
+            $0.adjustsFontSizeToFitWidth = true
+            $0.minimumScaleFactor = 0.5 // Adjust as needed
+            $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+            $0.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        }
+    }
+    
+    func configureLayout() {
+        NSLayoutConstraint.activate([
+            change.leftAnchor.constraint(equalTo: leftAnchor, constant: 8),
+            change.centerYAnchor.constraint(equalTo: centerYAnchor),
+            change.widthAnchor.constraint(equalToConstant: 10),
+            change.heightAnchor.constraint(equalToConstant: 20),
+            
+            coinName.bottomAnchor.constraint(equalTo: centerYAnchor, constant: 4),
+            coinName.leftAnchor.constraint(equalTo: change.rightAnchor, constant: 4),
+            
+            market.topAnchor.constraint(equalTo: centerYAnchor, constant: 4),
+            market.leftAnchor.constraint(equalTo: change.rightAnchor, constant: 4),
+            
+            fluctuationRate.rightAnchor.constraint(equalTo: rightAnchor, constant: -8),
+            fluctuationRate.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            priceFluctuation.rightAnchor.constraint(equalTo: rightAnchor, constant: -65),
+            priceFluctuation.bottomAnchor.constraint(equalTo: centerYAnchor),
+            
+            tradingVolume.topAnchor.constraint(equalTo: centerYAnchor),
+            tradingVolume.trailingAnchor.constraint(equalTo: priceFluctuation.trailingAnchor),
+            
+            price.centerXAnchor.constraint(equalTo: centerXAnchor),
+            price.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func configure(ticker: WebsocketTickerResponse) {
+        coinName.text = CoreDataManager.default.fetch(type: KRW.self)?.filter { $0.market == ticker.code }.first?.koreanName ?? ""
+        if ticker.change == "RISE" {
+            change.tintColor = .green
+            priceFluctuation.textColor = .green
+            fluctuationRate.textColor = .green
+        } else if ticker.change == "FALL" {
+            change.tintColor = .systemPink
+            priceFluctuation.textColor = .systemPink
+            fluctuationRate.textColor = .systemPink
+        } else {
+            change.tintColor = .gray
+            priceFluctuation.textColor = .gray
+            fluctuationRate.textColor = .gray
+        }
+        market.text = ticker.code
+        price.text = ticker.tradePrice.formattedWithSeparator
+        priceFluctuation.text = ticker.changePrice.formattedWithSeparator
+        tradingVolume.text = "\(ticker.accTradePrice24H.toPercentage(3))"
+        fluctuationRate.text = "\(ticker.signedChangeRate.toPercentage(2, 100))%"
+    }
