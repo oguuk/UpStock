ff1b79ad4d97b89393ad0a996bcb422e4ecc0fa6
oguuk <ogw135@gmail.com>
2023-08-14 20:25:20 +0900

    feat : ticker 가져오기

--- UpStock/UpStock/Manager/UpbitAPIManager.swift
+++ UpStock/UpStock/Manager/UpbitAPIManager.swift
@@ -18,3 +18,20 @@ final class UpbitAPIManager {
     
     private let disposeBag = DisposeBag()
     
+    func fetchTicker<T: Codable>(markets: String) -> Observable<[T]?> {
+        return Observable.create { observer in
+            let disposable = Network.default.get(url: Constant.baseURL + Constant.pathOfCurrentPrice, parameters: ["markets":markets])
+                .subscribe(onNext: { result in
+                    switch result {
+                    case let .success(data):
+                        print("DEBUG : \(String(data: data ?? Data(), encoding: .utf8))")
+                        self.handleSuccess(data: data, observer: observer)
+                    case let .failure(error):
+                        print("\(markets) error \(error.localizedDescription)")
+                        observer.onError(error)
+                    }
+                })
+            
+            return Disposables.create { disposable.dispose() }
+        }
+    }
