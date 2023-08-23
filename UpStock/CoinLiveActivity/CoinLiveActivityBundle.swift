a64eeb7553dd5b910be74eba6e52861d555f7ca9
oguuk <ogw135@gmail.com>
2023-08-23 23:56:28 +0900

    add : CoinLiveActivityBundle 추가

---
+++ UpStock/CoinLiveActivity/CoinLiveActivityBundle.swift
@@ -0,0 +1,17 @@
+//
+//  CoinLiveActivityBundle.swift
+//  CoinLiveActivity
+//
+//  Created by 오국원 on 2023/08/16.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CoinLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        CoinLiveActivity()
+        CoinLiveActivityLiveActivity()
+    }
+}
