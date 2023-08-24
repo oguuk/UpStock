632aaae7c87ca063c2613b5e737c04fc02c7e4f7
oguuk <ogw135@gmail.com>
2023-08-24 17:39:32 +0900

    feat : keyboard 입력 및 socket과 검색 결과 output

--- UpStock/UpStock/Presenter/Home/HomeViewModel.swift
+++ UpStock/UpStock/Presenter/Home/HomeViewModel.swift
@@ -14,3 +14,11 @@ import ActivityKit
 import CoinLiveActivityExtension
 
 final class HomeViewModel {
+    
+    struct Input {
+        let keyboard: Observable<String>
+    }
+    
+    struct Output {
+        let stocks: Driver<[TickerResponse]> //Observable<[SectionModel<String, WebsocketTickerResponse>]>
+    }
