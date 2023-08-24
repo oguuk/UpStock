bc4619070c276a293e6fdc8e809b4b7da48caa6d
oguuk <ogw135@gmail.com>
2023-08-24 16:04:54 +0900

    feat : CoreData Entity 프로퍼티에 접근을 위한 확장

---
+++ UpStock/UpStock/Util/Extension/KRW.swift
@@ -0,0 +1,23 @@
+//
+//  KRW.swift
+//  UpStock
+//
+//  Created by 오국원 on 2023/08/22.
+//
+
+import Foundation
+
+extension KRW: Nameable {
+    
+    var Market: String? {
+        return self.value(forKey: "market") as? String
+    }
+    
+    var KoreanName: String? {
+        return self.value(forKey: "koreanName") as? String
+    }
+    
+    var EnglishName: String? {
+        return self.value(forKey: "englishName") as? String
+    }
+}
