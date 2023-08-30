4f83966c7a9654250786a3514ad919b53f2092dd
oguuk <ogw135@gmail.com>
2023-08-30 23:34:30 +0900

    feat : candle 데이터 타입

---
+++ UpStock/UpStock/Domain/CandleData.swift
@@ -0,0 +1,39 @@
+//
+//  CandleData.swift
+//  UpStock
+//
+//  Created by 오국원 on 2023/08/02.
+//
+
+import Foundation
+
+typealias CandleDatas = [CandleData]
+
+struct CandleData: Codable {
+    let market, candleDateTimeUTC, candleDateTimeKst: String
+    let openingPrice, highPrice, lowPrice, tradePrice: Double
+    let timestamp, candleAccTradePrice, candleAccTradeVolume: Double
+    let prevClosingPrice: Double?
+    let changePrice: Double?
+    let changeRate: Double?
+    let unit: Int?
+    let firstDayOfPeriod: String?
+
+    enum CodingKeys: String, CodingKey {
+        case market
+        case candleDateTimeUTC = "candle_date_time_utc"
+        case candleDateTimeKst = "candle_date_time_kst"
+        case openingPrice = "opening_price"
+        case highPrice = "high_price"
+        case lowPrice = "low_price"
+        case tradePrice = "trade_price"
+        case timestamp
+        case candleAccTradePrice = "candle_acc_trade_price"
+        case candleAccTradeVolume = "candle_acc_trade_volume"
+        case prevClosingPrice = "prev_closing_price"
+        case changePrice = "change_price"
+        case changeRate = "change_rate"
+        case unit
+        case firstDayOfPeriod = "first_day_of_period"
+    }
+}
